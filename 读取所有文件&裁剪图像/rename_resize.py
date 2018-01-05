656b72c059f0660711a00776167df4e0bc0a309a
Se <liurole@qq.com>
2018-01-05 11:35:33 +0800

    实现了文件的裁剪功能，同时提供了怎么删除某一个文件夹下所有文件的方法

--- 读取所有文件&裁剪图像/rename_resize.py
+++ 读取所有文件&裁剪图像/rename_resize.py
@@ -6,12 +6,9 @@ Created on Fri Jan  5 09:30:44 2018
 """
 
 import os
+import cv2
 
-
-
-if __name__ == '__main__':
-    
-    data_dir = './data/'
+def rename(data_dir):
     
     index = 1
     list_dir = os.listdir(data_dir)
@@ -22,6 +19,46 @@ if __name__ == '__main__':
         file_names = file.split('.')
         new_name = str(index).zfill(length) + '.' + file_names[-1]
         os.rename(data_dir + file, data_dir + new_name)
-        index += 1
+        index += 1    
+          
+def delete_file_folder(src):
+    '''delete files and folders'''
+    if os.path.isfile(src):
+        try:
+            os.remove(src)
+        except:
+            pass
+    elif os.path.isdir(src):
+        for item in os.listdir(src):
+            itemsrc=os.path.join(src,item)
+            delete_file_folder(itemsrc)
+
+
+def resizeImg(in_dir, out_dir, width = 400, height = 300):
+    for file in os.listdir(in_dir):
+        name = in_dir + file
+        image = cv2.imread(name)
+        if image.shape[1]/image.shape[0] > width/height:
+            t_width = int(image.shape[1] / image.shape[0] * height)
+            temp = cv2.resize(image, (t_width, height), interpolation = cv2.INTER_CUBIC)
+            dest = temp[0:height, int(t_width/2 - width/2):int(t_width/2 - width/2) + width]
+            cv2.imwrite(out_dir + file, dest)
+        else:
+            t_height = int(image.shape[0] / image.shape[1] * width)
+            temp = cv2.resize(image, (width, t_height), interpolation = cv2.INTER_CUBIC)
+            dest = temp[int(t_height/2 - height/2):int(t_height/2 - height/2) + height, 0: width]
+            cv2.imwrite(out_dir + file, dest)            
+                
+
+if __name__ == '__main__':
+    
+    data_dir = './data/'
+    dest_dir = './dest/'
+    
+    #rename(data_dir)
+    delete_file_folder(dest_dir) 
+    resizeImg(data_dir, dest_dir)
+    
+
         
 
